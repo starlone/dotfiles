564c04632d915c46cefc58ddf6b2583f513ee5e0
Starlone <starlone.passos@gmail.com>
2017-07-31 11:13:57 -0300

    Add nodejs dependencies

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
 sudo apt update
 sudo apt full-upgrade
 
+# ----------
+# Shell
+# ----------
 dependencies=`cat dependencies.txt`
+sudo apt install -y $dependencies
 
-sudo apt install $dependencies
-
+echo '
+----------
+ Python
+----------
+'
 dep_python=`cat dependencies-python.txt`
-
 sudo easy_install -U $dep_python
+
+echo '
+----------
+ NodeJS
+----------
+'
+sudo npm install -g npm n yarn
+sudo n latest
+dep_nodejs=`cat dependencies-nodejs.txt`
+sudo npm install -g $dep_nodejs
