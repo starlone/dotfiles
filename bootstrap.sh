50ba0677e161c4c29d686d3647dfd7186c15ec76
Starlone <starlone.passos@gmail.com>
2017-07-31 12:55:46 -0300

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt install -y git
+
+if [ ! -d ~/git ]; then
+    mkdir -p ~/git
+fi
+
+cd ~/git
+
+if [ ! -d star.ubuntu-setup ]; then
+    git clone https://github.com/starlone/star.ubuntu-setup.git
+fi
+
+cd star.ubuntu-setup
+./setup.sh
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 sudo apt update
 sudo apt full-upgrade
 
