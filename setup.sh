ea5f118b73853a9ff99d02c32c05d736e7991037
Starlone <starlone.passos@gmail.com>
2017-11-10 21:50:42 -0200

    Separando as task em funcoes para que possam ser chamadas individualmente conforme parametro

--- setup.sh
+++ setup.sh
@@ -5,66 +5,107 @@ cd $BASEDIR
 
 git pull
 
-sudo apt update
-sudo apt -y full-upgrade
+taskshell() {
+    echo '
+    ----------
+    - Shell
+    ----------
+    '
+    sudo apt update
+    sudo apt -y full-upgrade
+    dependencies=`cat dependencies.txt`
+    sudo apt install -y $dependencies
+}
 
-echo '
-----------
-- Shell
-----------
-'
-dependencies=`cat dependencies.txt`
-sudo apt install -y $dependencies
+taskpython(){
+    echo '
+    ----------
+    - Python
+    ----------
+    '
+    dep_python=`cat dependencies-python.txt`
+    sudo easy_install -U $dep_python
+}
 
-echo '
-----------
-- Python
-----------
-'
-dep_python=`cat dependencies-python.txt`
-sudo easy_install -U $dep_python
+tasknodejs(){
+    echo '
+    ----------
+    - NodeJS
+    ----------
+    '
+    sudo npm install -g npm
+    sudo npm install -g n
+    # Install last nodejs
+    sudo n lts
+    dep_nodejs=`cat dependencies-nodejs.txt`
+    sudo npm install -g $dep_nodejs
+}
 
-echo '
-----------
-- NodeJS
-----------
-'
-sudo npm install -g npm
-sudo npm install -g n
-# Install last nodejs
-sudo n lts
-dep_nodejs=`cat dependencies-nodejs.txt`
-sudo npm install -g $dep_nodejs
+taskterminator(){
+    echo '
+    ----------
+    - Terminator
+    ----------
+    '
 
-echo '
-----------
-- Terminator
-----------
-'
+    if [ ! -d ~/.config/terminator ]; then
+        mkdir -p ~/.config/terminator
+    fi
+    ln -sf ~/git/star.ubuntu-setup/terminator_config ~/.config/terminator/config
+}
 
-if [ ! -d ~/.config/terminator ]; then
-    mkdir -p ~/.config/terminator
+taskvim(){
+    echo '
+    ----------
+    - VIM
+    ----------
+    '
+    mkdir -p ~/git
+    cd ~/git
+    if [ ! -d ~/git/starlone.vim ]; then
+        git clone https://github.com/starlone/starlone.vim.git
+        cd ~/
+        rm -rf .vimrc
+        ln -s ~/git/starlone.vim/vimrc .vimrc
+        vim +PlugInstall +qall
+    fi
+    cd ~/git/starlone.vim
+    git pull
+    vim +PlugUpgrade +qall
+    vim +PlugUpdate +qall
+    cd ~/.vim/plugged/YouCompleteMe
+    ./install.py --tern-completer
+}
+
+if [ $# -eq 0 ]; then
+    taskshell
+    taskterminator
+    taskpython
+    tasknodejs
+    taskvim
 fi
-ln -sf ~/git/star.ubuntu-setup/terminator_config ~/.config/terminator/config
 
-echo '
-----------
-- VIM
-----------
-'
-mkdir -p ~/git
-cd ~/git
-if [ ! -d ~/git/starlone.vim ]; then
-    git clone https://github.com/starlone/starlone.vim.git
-    cd ~/
-    rm -rf .vimrc
-    ln -s ~/git/starlone.vim/vimrc .vimrc
-    vim +PlugInstall +qall
-fi
-cd ~/git/starlone.vim
-git pull
-vim +PlugUpgrade +qall
-vim +PlugUpdate +qall
-cd ~/.vim/plugged/YouCompleteMe
-./install.py --tern-completer
+for PARAM in $*
+do
+    case $PARAM in
 
+        'shell')
+            taskshell
+            ;;
+        'python')
+            taskpython
+            ;;
+        'nodejs')
+            tasknodejs
+            ;;
+        'terminator')
+            taskterminator
+            ;;
+        'vim')
+            taskvim
+            ;;
+        *)
+            echo "Não existe esta opção! " $PARAM "\n"
+            ;;
+    esac
+done
