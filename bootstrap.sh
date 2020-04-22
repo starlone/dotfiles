d99680239bf0c52819b71b6154704d6ba586cdc4
Starlone <starlone.passos@gmail.com>
2020-04-22 14:39:06 -0300

    bootstrap - update path

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,8 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt install -y git
 
-if [ ! -d ~/git/dotfiles ]; then
-    mkdir -p ~/git
-    git clone https://github.com/starlone/dotfiles.git ~/git/dotfiles
+if [ ! -d ~/dotfiles ]; then
+    git clone https://github.com/starlone/dotfiles.git ~/dotfiles
 fi
 
-~/git/dotfiles/install.sh
+~/dotfiles/install.sh
